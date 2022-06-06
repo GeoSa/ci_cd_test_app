8c062df25fff439a2db781d70bf8364e602cec8f
geosa <sakhabutdinovga@yandex.ru>
2022-06-06 15:47:57 +0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
+user = os.getenv('DB_USER')
+print(f'user - {user}')
 
 
 @app.route('/', methods=['GET'])
@@ -11,6 +19,17 @@ def hello_world() -> str:
 @app.route('/ping/', methods=['GET'])
 def ping() -> str:
     return 'Pong!'
+
+
+@app.route('/write/', methods=['GET'])
+def write() -> str:
+    try:
+        with open('data/data.txt', 'w') as file:
+            file.write('Hello')
+
+        return 'Done'
+    except Exception as err:
+        return str(err)
 
 
 if __name__ == '__main__':
